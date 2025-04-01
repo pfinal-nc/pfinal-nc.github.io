b36e8b81d4559a482ab5b0aa5594bafee74db227
pfinal-nc <pfinal-nc@users.noreply.github.com>
2025-04-01 15:50:17 +0800

    deploy: update site d5fdf16eca5ee05d81ae145559bffeb1689137f5

---
+++ assets/php_PHP-FPM配置文件详解.md.B1Q5_FoX.js
@@ -0,0 +1,136 @@
+import{_ as n,c as a,o as p,a6 as e}from"./chunks/framework.B5rgnJXo.js";const m=JSON.parse('{"title":"PHP-FPM配置文件解析","description":"详细介绍PHP-FPM配置文件解析，包括配置文件的基本结构、常用配置项、进程管理、日志记录等内容，帮助开发者更好地配置和管理PHP-FPM服务。","frontmatter":{"title":"PHP-FPM配置文件解析","date":"2023-04-27T22:10:20.000Z","author":"PFinal南丞","tags":["PHP"],"keywords":"PHP-FPM配置文件解析, php, 工具, php-fpm配置文件, php-fpm配置文件解析, php-fpm配置文件详解,PHP异步处理, php, 工具, php异步处理, php协程, php异步协程,PHP, 进程, 线程, 解析, PHP进程, PHP线程, PHP进程线程, PHP进程线程解析, PHP进程线程解析,PHP, 配置文件, 详解, 编程, web开发, 服务器配置,PHP钩子, php, 工具, php钩子, php钩子应用, php钩子的实现,Redis, 基本知识, 总结, 数据库, NoSQL, 缓存, 计数器, 排行榜, 社交网络, 消息队列, 分布式锁,Redis配置文件详解, 数据库, redis,TCP/IP,HTTP,Python, 库, 函数, 常用, Faker, 生成, 数据, 测试, 模拟,Python, 协程, 并发, 编程, asyncio, 异步IO, Future, Task,Python, 网站, 搜集, 资源, 开发, 编程, 工具, 镜像站, 爬虫,Python版本管理神器之pyenv, python, 工具, python版本管理, pyenv使用教程, python环境配置,爬虫, 加密, 解密, 算法, 特征, 收集,爬虫JS逆向Webpack技巧记录, 爬虫, JS, Webpack,爬虫, 加密, 解密, 算法, 特征, 收集,Composer, 配置文件, 依赖管理, 包管理, 版本控制,PHP, $_SERVER,PHP, 会话, 引发, Bug, PHP_SESSION, $_SESSION, 会话管理,PHP, 生成器, 解析, PHP生成器, PHP生成器解析, PHP生成器解析,PHP, 错误与异常处理, 异常, 错误, 处理, 异常处理, 错误处理, 程序, 健壮性, 稳定性,golang, gopsutil, 系统库, 监控, 系统信息, 进程, 进程监控, 系统信息获取,使用Go实现服务端事件推送SSE, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,用Wails和Vue.js打造跨平台桌面应用程序, Wails, Vue.js, 跨平台桌面应用程序, 抖音, 直播,基于Wails的Mac桌面应用开发, golang, Wails, 桌面应用, 开发,基于Wails的抖音直播工具, golang, Wails, 抖音, 直播, 工具, 桌面应用,golang, 游戏开发, 接口设计, 高性能, 高并发, 游戏服务器, 游戏接口, 游戏框架,Wails, 应用, 开发, 尝试, 桌面应用, Go, Web开发, Tailwindcss, sqlite,Go-Cache, Wails, 内存缓存, 性能优化, 缓存策略, 内存管理, 应用程序性能,golang, 工具, 提升生产力, 开发, golang, Go开发, Go工具, Go扩展包,提速利器：使用Go语言实现静态化API, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,深入Go内存分配, golang, 内存分配,Go Channel, 批量读取, 实际应用,Laravel, 构建, 手工, 框架, 手工构建, PHP,Laravel, Carbon, 类, 使用, Laravel-Carbon-类使用, Laravel-Carbon-类使用解析,PHP, 进程, 线程, 解析, PHP进程, PHP线程, PHP进程线程, PHP进程线程解析, PHP进程线程解析,golang, TLS, SSL, 网络安全, 传输层安全协议, 安全套接层, 加密, 数据传输, 网络通信,Golang, 协程池, 实现, 方法, 协程, 池, 概念, 应用, 场景, 实现方式, 协程池功能, 性能, 响应速度,golang, 脱敏扩展包：简化敏感信息处理的利器, 工具, golang扩展包, golang脱敏, golang脱敏工具,Golang, 工具, 效率, 提升, 小工具, 代码质量, 开发, 编程, 工具, 自动格式化, 自动导入, 代码补全,undefined,AutoCorrent专有名词大小写扩展包, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,Create Go App CLI, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,Go语言开发终端小工具后续, Go, 工具, 终端, 小工具, 开发, 编程, 命令, 工具开发, 代码实现, 运行命令,GO开发IP过滤小脚本, go, 工具, go开发ip过滤, go ip过滤脚本, go ip过滤,Go 开发终端小工具, golang, 工具, 开发, 编程, 终端, 天气查询, 手机归属地查询, cobra库, 命令行小工具,Go语言的高性能User-Agent解析库, golang, 工具, User-Agent解析, 性能测试, 解析库,golang, 地址生成, 扩展包, 地理位置, 中国地址, 地址生成工具, 地址数据, 地址库,Gomail邮件发送包, golang, 工具, 邮件发送, 邮件接收, 邮件发送包, 邮件发送工具,Go, URL检测, 工具, 编程, 终端命令, 开发, 检测, 小工具, 终端命令小工具, Go终端命令小工具,Go语言安全库使用指南, Go语言, 安全库, 使用指南, crypto, encoding, hash, math, rand, strconv, time,Go语言实现守护进程, 守护进程, 技术详解, Go语言, 进程管理, 关键技术点,Go, 并发, 模式, 编程, 并发编程, Go语言, 并发模式, 并发实战, 并发指南,go:embed 在 Go 开发中的应用与最佳实践, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,Golang, Web应用, 安全指南, 输入验证, 输出编码, 会话管理, 文件上传, 跨站脚本攻击, SQL注入, 密码存储, 身份验证, 授权, 安全配置, 日志记录, 错误处理, 性能优化, 安全审计,pfinalclub, git, gitsite, javascript, node, jquery, python, php, laravel, sql, database, linux, operating system, os, cpu, verilog, risc-v, bitcoin, ethereum, ai, 教程, 软件, 编程, 开发, 运维, 云计算, 网络, 互联网, 比特币, 以太坊, 操作系统, 智能合约, 数字货币, 爬虫, 逆向","description":"详细介绍PHP-FPM配置文件解析，包括配置文件的基本结构、常用配置项、进程管理、日志记录等内容，帮助开发者更好地配置和管理PHP-FPM服务。"},"headers":[],"relativePath":"php/PHP-FPM配置文件详解.md","filePath":"php/PHP-FPM配置文件详解.md","lastUpdated":1741950068000}'),l={name:"php/PHP-FPM配置文件详解.md"};function i(o,s,t,c,r,h){return p(),a("div",{"data-pagefind-body":!0},s[0]||(s[0]=[e(`<h1 id="php-fpm配置文件详解" tabindex="-1">PHP-FPM配置文件详解 <a class="header-anchor" href="#php-fpm配置文件详解" aria-label="Permalink to &quot;PHP-FPM配置文件详解&quot;">​</a></h1><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>[www]</span></span>
+<span class="line"><span>; It only applies on the following directives:</span></span>
+<span class="line"><span>; - &#39;access.log&#39;</span></span>
+<span class="line"><span>; - &#39;slowlog&#39;</span></span>
+<span class="line"><span>; - &#39;listen&#39; (unixsocket)</span></span>
+<span class="line"><span>; - &#39;chroot&#39;</span></span>
+<span class="line"><span>; - &#39;chdir&#39;</span></span>
+<span class="line"><span>; - &#39;php_values&#39;</span></span>
+<span class="line"><span>; - &#39;php_admin_values&#39;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>;prefix = /path/to/pools/$pool   如果没有制定，将使用全局prefix替代</span></span>
+<span class="line"><span>user = nobody　　　　　　　　　　　　 进程的发起用户和用户组，用户user是必须设置，group不是</span></span>
+<span class="line"><span>group = nobody</span></span>
+<span class="line"><span>listen = 127.0.0.1:9000　　　　　　 监听ip和端口</span></span>
+<span class="line"><span>;listen.backlog = 65535　　　　　　 设置listen(2)函数backlog</span></span>
+<span class="line"><span>;listen.owner = nobody</span></span>
+<span class="line"><span>;listen.group = nobody</span></span>
+<span class="line"><span>;listen.mode = 0660</span></span>
+<span class="line"><span>;listen.acl_users =</span></span>
+<span class="line"><span>;listen.acl_groups =</span></span>
+<span class="line"><span>;listen.allowed_clients = 127.0.0.1 允许FastCGI客户端连接的IPv4地址，多个地址用&#39;,&#39;分隔，为空则允许任何地址发来链接请求</span></span>
+<span class="line"><span>; process.priority = -19</span></span>
+<span class="line"><span>pm = dynamic　　　　　　　　　　　　　 选择进程池管理器如何控制子进程的数量</span></span>
+<span class="line"><span>　　static：　　对于子进程的开启数路给定一个锁定的值(pm.max_children)</span></span>
+<span class="line"><span>　　dynamic:　 子进程的数目为动态的，它的数目基于下面的指令的值(以下为dynamic适用参数)</span></span>
+<span class="line"><span>　　　　pm.max_children：  同一时刻能够存货的最大子进程的数量</span></span>
+<span class="line"><span>　　　　pm.start_servers： 在启动时启动的子进程数量</span></span>
+<span class="line"><span>　　　　pm.min_spare_servers： 处于空闲&quot;idle&quot;状态的最小子进程，如果空闲进程数量小于这个值，那么相应的子进程会被创建</span></span>
+<span class="line"><span>　　　　pm.max_spare_servers： 最大空闲子进程数量，空闲子进程数量超过这个值，那么相应的子进程会被杀掉。</span></span>
+<span class="line"><span>　　ondemand： 在启动时不会创建，只有当发起请求链接时才会创建(pm.max_children, pm.process_idle_timeout)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>pm.max_children = 5</span></span>
+<span class="line"><span>pm.start_servers = 2</span></span>
+<span class="line"><span>pm.min_spare_servers = 1</span></span>
+<span class="line"><span>pm.max_spare_servers = 3</span></span>
+<span class="line"><span>;pm.process_idle_timeout = 10s;　　空闲进程超时时间</span></span>
+<span class="line"><span>;pm.max_requests = 500　　　　　　　 在派生新的子进程前，每一个子进程应该处理的请求数目，在第三方库中解决内存溢出很有用，设置为0则不会限制</span></span>
+<span class="line"><span>;pm.status_path = /status　　　     配置一个URI，以便查看fpm状态页</span></span>
+<span class="line"><span>状态页描述：</span></span>
+<span class="line"><span>　　accepted conn: 该进程池接受的请求数量</span></span>
+<span class="line"><span>　　pool: 进程池的名字</span></span>
+<span class="line"><span>　　process manager: 进程管理，就是配置中pm指令，可以选择值static，dynamic，ondemand</span></span>
+<span class="line"><span>　　idle processes: 空闲进程数量</span></span>
+<span class="line"><span>　　active processes: 当前活跃的进程数量</span></span>
+<span class="line"><span>　　total processes: 总的进程数量=idle+active</span></span>
+<span class="line"><span>　　max children reached: 达到最大子进程的次数，达到进程的限制，当pm试图开启更多的子进程的时候(仅当pm工作在dynamic时)</span></span>
+<span class="line"><span>;ping.path = /ping　　　　该ping URI将会去调用fpm监控页面，如果这个没有设置，那么不会有URI被做为ping页</span></span>
+<span class="line"><span>;ping.response = pong　　用于定制平请求的响应，响应的格式text/plain(对200响应代码)</span></span>
+<span class="line"><span>;access.log = log/$pool.access.log</span></span>
+<span class="line"><span>;access.format = &quot;%R - %u %t \\&quot;%m %r%Q%q\\&quot; %s %f %{mili}d %{kilo}M %C%%&quot;</span></span>
+<span class="line"><span>　　; The following syntax is allowed</span></span>
+<span class="line"><span>　　;  %%: the &#39;%&#39; character</span></span>
+<span class="line"><span>　　;  %C: %CPU used by the request</span></span>
+<span class="line"><span>　　;      it can accept the following format:</span></span>
+<span class="line"><span>　　;      - %{user}C for user CPU only</span></span>
+<span class="line"><span>　　;      - %{system}C for system CPU only</span></span>
+<span class="line"><span>　　;      - %{total}C  for user + system CPU (default)</span></span>
+<span class="line"><span>　　;  %d: time taken to serve the request</span></span>
+<span class="line"><span>　　;      it can accept the following format:</span></span>
+<span class="line"><span>　　;      - %{seconds}d (default)</span></span>
+<span class="line"><span>　　;      - %{miliseconds}d</span></span>
+<span class="line"><span>　　;      - %{mili}d</span></span>
+<span class="line"><span>　　;      - %{microseconds}d</span></span>
+<span class="line"><span>　　;      - %{micro}d</span></span>
+<span class="line"><span>　　;  %e: an environment variable (same as $_ENV or $_SERVER)</span></span>
+<span class="line"><span>　　;      it must be associated with embraces to specify the name of the env</span></span>
+<span class="line"><span>　　;      variable. Some exemples:</span></span>
+<span class="line"><span>　　;      - server specifics like: %{REQUEST_METHOD}e or %{SERVER_PROTOCOL}e</span></span>
+<span class="line"><span>　　;      - HTTP headers like: %{HTTP_HOST}e or %{HTTP_USER_AGENT}e</span></span>
+<span class="line"><span>　　;  %f: script filename</span></span>
+<span class="line"><span>　　;  %l: content-length of the request (for POST request only)</span></span>
+<span class="line"><span>　　;  %m: request method</span></span>
+<span class="line"><span>　　;  %M: peak of memory allocated by PHP</span></span>
+<span class="line"><span>　　;      it can accept the following format:</span></span>
+<span class="line"><span>　　;      - %{bytes}M (default)</span></span>
+<span class="line"><span>　　;      - %{kilobytes}M</span></span>
+<span class="line"><span>　　;      - %{kilo}M</span></span>
+<span class="line"><span>　　;      - %{megabytes}M</span></span>
+<span class="line"><span>　　;      - %{mega}M</span></span>
+<span class="line"><span>　　;  %n: pool name</span></span>
+<span class="line"><span>　　;  %o: output header</span></span>
+<span class="line"><span>　　;      it must be associated with embraces to specify the name of the header:</span></span>
+<span class="line"><span>　　;      - %{Content-Type}o</span></span>
+<span class="line"><span>　　;      - %{X-Powered-By}o</span></span>
+<span class="line"><span>　　;      - %{Transfert-Encoding}o</span></span>
+<span class="line"><span>　　;      - ....</span></span>
+<span class="line"><span>　　;  %p: PID of the child that serviced the request</span></span>
+<span class="line"><span>　　;  %P: PID of the parent of the child that serviced the request</span></span>
+<span class="line"><span>　　;  %q: the query string</span></span>
+<span class="line"><span>　　;  %Q: the &#39;?&#39; character if query string exists</span></span>
+<span class="line"><span>　　;  %r: the request URI (without the query string, see %q and %Q)</span></span>
+<span class="line"><span>　　;  %R: remote IP address</span></span>
+<span class="line"><span>　　;  %s: status (response code)</span></span>
+<span class="line"><span>　　;  %t: server time the request was received</span></span>
+<span class="line"><span>　　;      it can accept a strftime(3) format:</span></span>
+<span class="line"><span>　　;      %d/%b/%Y:%H:%M:%S %z (default)</span></span>
+<span class="line"><span>　　;  %T: time the log has been written (the request has finished)</span></span>
+<span class="line"><span>　　;      it can accept a strftime(3) format:</span></span>
+<span class="line"><span>　　;      %d/%b/%Y:%H:%M:%S %z (default)</span></span>
+<span class="line"><span>　　;  %u: remote user</span></span>
+<span class="line"><span>;slowlog = log/$pool.log.slow　　 用于记录慢请求</span></span>
+<span class="line"><span>;request_slowlog_timeout = 0　　  慢日志请求超时时间，对一个php程序进行跟踪。</span></span>
+<span class="line"><span>;request_terminate_timeout = 0　　终止请求超时时间，在worker进程被杀掉之后，提供单个请求的超时间隔。由于某种原因不停止脚本执行时，应该使用该选项，0表示关闭不启用</span></span>
+<span class="line"><span>　　(在php.ini中，max_execution_time 一般设置为30，表示每一个脚本的最大执行时间)</span></span>
+<span class="line"><span>;rlimit_files = 1024　　　　　　　　设置打开文件描述符的限制</span></span>
+<span class="line"><span>;rlimit_core = 0　　　　　　　　　　 设置内核对资源的使用限制，用于内核转储</span></span>
+<span class="line"><span>;chroot =　　　　　　　　　　　　　　　设置chroot路径，程序一启动就将其chroot放置到指定的目录下，该指令值必须是一个绝对路径</span></span>
+<span class="line"><span>;chdir = /var/www　　　　　　　　　　在程序启动时将会改变到指定的位置(这个是相对路径，相对当前路径或chroot后的“/”目录)　　　　</span></span>
+<span class="line"><span>;catch_workers_output = yes　　　　将worker的标准输出和错误输出重定向到主要的错误日志记录中，如果没有设置，根据FastCGI的指定，将会被重定向到/dev/null上</span></span>
+<span class="line"><span>;clear_env = no　　　　　　　　　　  清理环境</span></span>
+<span class="line"><span>;security.limit_extensions = .php .php3 .php4 .php5　　限制FPM执行解析的扩展名</span></span>
+<span class="line"><span>;env[HOSTNAME] = $HOSTNAME</span></span>
+<span class="line"><span>;env[PATH] = /usr/local/bin:/usr/bin:/bin</span></span>
+<span class="line"><span>;env[TMP] = /tmp</span></span>
+<span class="line"><span>;env[TMPDIR] = /tmp</span></span>
+<span class="line"><span>;env[TEMP] = /tmp</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>; Additional php.ini defines, specific to this pool of workers. These settings</span></span>
+<span class="line"><span>; overwrite the values previously defined in the php.ini. The directives are the</span></span>
+<span class="line"><span>; same as the PHP SAPI:</span></span>
+<span class="line"><span>;   php_value/php_flag             - you can set classic ini defines which can</span></span>
+<span class="line"><span>;                                    be overwritten from PHP call &#39;ini_set&#39;.</span></span>
+<span class="line"><span>;   php_admin_value/php_admin_flag - these directives won&#39;t be overwritten by</span></span>
+<span class="line"><span>;                                     PHP call &#39;ini_set&#39;</span></span>
+<span class="line"><span>; For php_*flag, valid values are on, off, 1, 0, true, false, yes or no.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>; Defining &#39;extension&#39; will load the corresponding shared extension from</span></span>
+<span class="line"><span>; extension_dir. Defining &#39;disable_functions&#39; or &#39;disable_classes&#39; will not</span></span>
+<span class="line"><span>; overwrite previously defined php.ini values, but will append the new value</span></span>
+<span class="line"><span>; instead.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>;php_admin_value[sendmail_path] = /usr/sbin/sendmail -t -i -f www@my.domain.com</span></span>
+<span class="line"><span>;php_flag[display_errors] = off</span></span>
+<span class="line"><span>;php_admin_value[error_log] = /var/log/fpm-php.www.log</span></span>
+<span class="line"><span>;php_admin_flag[log_errors] = on</span></span>
+<span class="line"><span>;php_admin_value[memory_limit] = 32M</span></span></code></pre></div>`,2)]))}const P=n(l,[["render",i]]);export{m as __pageData,P as default};
---
+++ assets/php_PHP配置文件详解.md.DiX17Xsg.js
@@ -0,0 +1,74 @@
+import{_ as n,c as a,o as p,a6 as e}from"./chunks/framework.B5rgnJXo.js";const _=JSON.parse('{"title":"PHP配置文件详解","description":"PHP配置文件详解","frontmatter":{"title":"PHP配置文件详解","date":"2022-07-04T13:36:42.000Z","tags":["PHP"],"description":"PHP配置文件详解","author":"PFinal南丞","keywords":"PHP, 配置文件, 详解, 编程, web开发, 服务器配置,PHP钩子, php, 工具, php钩子, php钩子应用, php钩子的实现,Redis, 基本知识, 总结, 数据库, NoSQL, 缓存, 计数器, 排行榜, 社交网络, 消息队列, 分布式锁,Redis配置文件详解, 数据库, redis,TCP/IP,HTTP,Python, 库, 函数, 常用, Faker, 生成, 数据, 测试, 模拟,Python, 协程, 并发, 编程, asyncio, 异步IO, Future, Task,Python, 网站, 搜集, 资源, 开发, 编程, 工具, 镜像站, 爬虫,Python版本管理神器之pyenv, python, 工具, python版本管理, pyenv使用教程, python环境配置,爬虫, 加密, 解密, 算法, 特征, 收集,爬虫JS逆向Webpack技巧记录, 爬虫, JS, Webpack,爬虫, 加密, 解密, 算法, 特征, 收集,Composer, 配置文件, 依赖管理, 包管理, 版本控制,PHP, $_SERVER,PHP, 会话, 引发, Bug, PHP_SESSION, $_SESSION, 会话管理,PHP, 生成器, 解析, PHP生成器, PHP生成器解析, PHP生成器解析,PHP, 错误与异常处理, 异常, 错误, 处理, 异常处理, 错误处理, 程序, 健壮性, 稳定性,golang, gopsutil, 系统库, 监控, 系统信息, 进程, 进程监控, 系统信息获取,使用Go实现服务端事件推送SSE, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,用Wails和Vue.js打造跨平台桌面应用程序, Wails, Vue.js, 跨平台桌面应用程序, 抖音, 直播,基于Wails的Mac桌面应用开发, golang, Wails, 桌面应用, 开发,基于Wails的抖音直播工具, golang, Wails, 抖音, 直播, 工具, 桌面应用,golang, 游戏开发, 接口设计, 高性能, 高并发, 游戏服务器, 游戏接口, 游戏框架,Wails, 应用, 开发, 尝试, 桌面应用, Go, Web开发, Tailwindcss, sqlite,Go-Cache, Wails, 内存缓存, 性能优化, 缓存策略, 内存管理, 应用程序性能,golang, 工具, 提升生产力, 开发, golang, Go开发, Go工具, Go扩展包,提速利器：使用Go语言实现静态化API, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,深入Go内存分配, golang, 内存分配,Go Channel, 批量读取, 实际应用,Laravel, 构建, 手工, 框架, 手工构建, PHP,Laravel, Carbon, 类, 使用, Laravel-Carbon-类使用, Laravel-Carbon-类使用解析,PHP, 进程, 线程, 解析, PHP进程, PHP线程, PHP进程线程, PHP进程线程解析, PHP进程线程解析,golang, TLS, SSL, 网络安全, 传输层安全协议, 安全套接层, 加密, 数据传输, 网络通信,Golang, 协程池, 实现, 方法, 协程, 池, 概念, 应用, 场景, 实现方式, 协程池功能, 性能, 响应速度,golang, 脱敏扩展包：简化敏感信息处理的利器, 工具, golang扩展包, golang脱敏, golang脱敏工具,Golang, 工具, 效率, 提升, 小工具, 代码质量, 开发, 编程, 工具, 自动格式化, 自动导入, 代码补全,undefined,AutoCorrent专有名词大小写扩展包, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,Create Go App CLI, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,Go语言开发终端小工具后续, Go, 工具, 终端, 小工具, 开发, 编程, 命令, 工具开发, 代码实现, 运行命令,GO开发IP过滤小脚本, go, 工具, go开发ip过滤, go ip过滤脚本, go ip过滤,Go 开发终端小工具, golang, 工具, 开发, 编程, 终端, 天气查询, 手机归属地查询, cobra库, 命令行小工具,Go语言的高性能User-Agent解析库, golang, 工具, User-Agent解析, 性能测试, 解析库,golang, 地址生成, 扩展包, 地理位置, 中国地址, 地址生成工具, 地址数据, 地址库,Gomail邮件发送包, golang, 工具, 邮件发送, 邮件接收, 邮件发送包, 邮件发送工具,Go, URL检测, 工具, 编程, 终端命令, 开发, 检测, 小工具, 终端命令小工具, Go终端命令小工具,Go语言安全库使用指南, Go语言, 安全库, 使用指南, crypto, encoding, hash, math, rand, strconv, time,Go语言实现守护进程, 守护进程, 技术详解, Go语言, 进程管理, 关键技术点,Go, 并发, 模式, 编程, 并发编程, Go语言, 并发模式, 并发实战, 并发指南,go:embed 在 Go 开发中的应用与最佳实践, golang, 项目创建, 快速创建, 工具, 项目, 快速, 工具,Golang, Web应用, 安全指南, 输入验证, 输出编码, 会话管理, 文件上传, 跨站脚本攻击, SQL注入, 密码存储, 身份验证, 授权, 安全配置, 日志记录, 错误处理, 性能优化, 安全审计,pfinalclub, git, gitsite, javascript, node, jquery, python, php, laravel, sql, database, linux, operating system, os, cpu, verilog, risc-v, bitcoin, ethereum, ai, 教程, 软件, 编程, 开发, 运维, 云计算, 网络, 互联网, 比特币, 以太坊, 操作系统, 智能合约, 数字货币, 爬虫, 逆向"},"headers":[],"relativePath":"php/PHP配置文件详解.md","filePath":"php/PHP配置文件详解.md","lastUpdated":1741944848000}'),l={name:"php/PHP配置文件详解.md"};function i(o,s,c,t,r,P){return p(),a("div",{"data-pagefind-body":!0},s[0]||(s[0]=[e(`<h1 id="php配置文件详解" tabindex="-1">PHP配置文件详解 <a class="header-anchor" href="#php配置文件详解" aria-label="Permalink to &quot;PHP配置文件详解&quot;">​</a></h1><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>; 避免PHP信息暴露在http头中</span></span>
+<span class="line"><span>expose_php = Off</span></span>
+<span class="line"><span>; 避免暴露php调用mysql的错误信息</span></span>
+<span class="line"><span>display_errors = Off</span></span>
+<span class="line"><span>; 在关闭display_errors后开启PHP错误日志（路径在php-fpm.conf中配置）</span></span>
+<span class="line"><span>log_errors = On</span></span>
+<span class="line"><span>; 设置PHP的扩展库路径</span></span>
+<span class="line"><span>extension_dir = &quot;/usr/local/php7/lib/php/extensions/no-debug-non-zts-20141001/&quot;</span></span>
+<span class="line"><span>; 设置PHP的opcache和mysql动态库</span></span>
+<span class="line"><span>zend_extension=opcache.so</span></span>
+<span class="line"><span>extension=mysqli.so</span></span>
+<span class="line"><span>extension=pdo_mysql.so</span></span>
+<span class="line"><span>; 设置PHP的时区</span></span>
+<span class="line"><span>date.timezone = PRC</span></span>
+<span class="line"><span>; 开启opcache</span></span>
+<span class="line"><span>[opcache]</span></span>
+<span class="line"><span>; Determines if Zend OPCache is enabled</span></span>
+<span class="line"><span>opcache.enable=1</span></span>
+<span class="line"><span>;  设置PHP脚本允许访问的目录（需要根据实际情况配置）</span></span>
+<span class="line"><span>;open_basedir = /usr/share/nginx/html;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>;;;;;;;;;;;;;;;; </span></span>
+<span class="line"><span>; File Uploads ; </span></span>
+<span class="line"><span>;;;;;;;;;;;;;;;; </span></span>
+<span class="line"><span>file_uploads = On ; 是否允许HTTP方式文件上载 </span></span>
+<span class="line"><span>;upload_tmp_dir = ; 用于HTTP上载的文件的临时目录（未指定则使用系统默认） </span></span>
+<span class="line"><span>upload_max_filesize = 2M ; 上载文件的最大许可大小</span></span>
+<span class="line"><span>post_max_size = 128M</span></span>
+<span class="line"><span>upload_max_filesize = 128M</span></span>
+<span class="line"><span>; 这两个设置一样即可，可以更大但要注意超时</span></span>
+<span class="line"><span>max_execution_time = 30</span></span>
+<span class="line"><span>max_input_time = 600</span></span>
+<span class="line"><span>memory_limit = 32M</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>; 上传大文件 </span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>; Fopen wrappers ; </span></span>
+<span class="line"><span>;;;;;;;;;;;;;;;;;; </span></span>
+<span class="line"><span>allow_url_fopen = On ; 是否允许把URLs当作http:.. 或把文件当作ftp:...</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>[Debugger] </span></span>
+<span class="line"><span>debugger.host = localhost </span></span>
+<span class="line"><span>debugger.port = 7869 </span></span>
+<span class="line"><span>debugger.enabled = False</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>[Session] </span></span>
+<span class="line"><span>session.save_handler = files ; 用于保存/取回数据的控制方式 </span></span>
+<span class="line"><span>session.save_path = C:\\win\\temp ; 在 save_handler 设为文件时传给控制器的参数， </span></span>
+<span class="line"><span>; 这是数据文件将保存的路径。 </span></span>
+<span class="line"><span>session.use_cookies = 1 ; 是否使用cookies </span></span>
+<span class="line"><span>session.name = PHPSESSID </span></span>
+<span class="line"><span>; 用在cookie里的session的名字 </span></span>
+<span class="line"><span>session.auto_start = 0 ; 在请求启动时初始化session </span></span>
+<span class="line"><span>session.cookie_lifetime = 0 ; 为按秒记的cookie的保存时间， </span></span>
+<span class="line"><span>; 或为0时，直到浏览器被重启 </span></span>
+<span class="line"><span>session.cookie_path = / ; cookie的有效路径 </span></span>
+<span class="line"><span>session.cookie_domain = ; cookie的有效域 </span></span>
+<span class="line"><span>session.serialize_handler = php ; 用于连接数据的控制器 </span></span>
+<span class="line"><span>; php是 PHP 的标准控制器。 </span></span>
+<span class="line"><span>session.gc_probability = 1 ; 按百分比的&#39;garbage collection（碎片整理）&#39;进程 </span></span>
+<span class="line"><span>; 在每次 session 初始化的时候开始的可能性。 </span></span>
+<span class="line"><span>session.gc_maxlifetime = 1440 ; 在这里数字所指的秒数后，保存的数据将被视为 </span></span>
+<span class="line"><span>; &#39;碎片(garbage)&#39;并由gc 进程清理掉。 </span></span>
+<span class="line"><span>session.referer_check = ; 检查 HTTP引用以使额外包含于URLs中的ids无效 </span></span>
+<span class="line"><span>session.entropy_length = 0 ; 从文件中读取多少字节 </span></span>
+<span class="line"><span>session.entropy_file = ; 指定这里建立 session id </span></span>
+<span class="line"><span>; session.entropy_length = 16 </span></span>
+<span class="line"><span>; session.entropy_file = /dev/urandom </span></span>
+<span class="line"><span>session.cache_limiter = nocache ; 设为{nocache,private,public},以决定 HTTP 的 </span></span>
+<span class="line"><span>; 缓存问题 </span></span>
+<span class="line"><span>session.cache_expire = 180 ; 文档在 n 分钟后过时 </span></span>
+<span class="line"><span>session.use_trans_sid = 1 ; 使用过渡性的 sid 支持，若编译时许可了 </span></span>
+<span class="line"><span>; --enable-trans-sid </span></span>
+<span class="line"><span>url_rewriter.tags = &quot; a=href,area=href,frame=src,input=src,form=fakeentry&quot;</span></span></code></pre></div>`,2)]))}const d=n(l,[["render",i]]);export{_ as __pageData,d as default};
